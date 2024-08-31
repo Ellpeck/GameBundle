a6600a1c84153016a4ef9c60a582539b5c20e9ab
Ellpeck <me@ellpeck.de>
2024-08-31 13:18:09 +0200

    updated to .net 8

--- Test/Bundle.bat
+++ Test/Bundle.bat
@@ -1,2 +1,3 @@
+dotnet build ../GameBundle/GameBundle.csproj
 rmdir /S /Q "bin/Bundled"
-"../GameBundle/bin/Debug/net6.0/GameBundle.exe" -wlmWL -bznV -s Test.csproj -o bin/Bundled -v --mac-bundle-ignore macmain.txt -N beta
+"../GameBundle/bin/Debug/net8.0/GameBundle.exe" -wlmWL -bnV -s Test.csproj -o bin/Bundled -v --mac-bundle-ignore macmain.txt -N beta
